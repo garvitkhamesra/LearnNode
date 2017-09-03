1117bd46e1403cc4149fbd208aac0d9220256103
garvit khamesra <garvit khamesra>
2017-09-03 12:48:41 +0530

    Exports and own modules

--- node_firstApp/app.js
+++ node_firstApp/app.js
@@ -2,15 +2,22 @@ console.log("Staring Application");
 
 const os = require('os');
 const fs = require('fs');
+const note = require('./note.js');
 
 var user = os.userInfo();
 
 //Template String
-fs.appendFile('helloworld.txt',`Hello ${user.username} !`, function (err) {
+fs.appendFile('helloworld.txt',`Hello ${user.username}t! You are a young ${note.age} year old man.`, function (err) {
     if(err){
         console.log("Sorry, Unable to write to the file");
     }    
 });
+
+var res = note.addNote();
+console.log(res);
+
+var sum = note.Sum(5,6);
+console.log('Sum: '+sum);
 
 // fs.appendFileSync('filename','txt');
 // for synchronous
---
+++ node_firstApp/note.js
@@ -0,0 +1,12 @@
+console.log('Starting Node.js');
+
+module.exports.age = 20;
+
+module.exports.addNote = () => {
+    console.log("Add Node");
+    return "New Node";
+};
+
+module.exports.Sum = (a,b) => {
+    return a+b;
+};
