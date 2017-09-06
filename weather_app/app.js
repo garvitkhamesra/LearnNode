21cbc63160d7c48b64f2ffaaa3b87cef8d183d39
Garvit Khamesra <garvitkhamesra@gmail.com>
2017-09-07 00:01:27 +0530

    geolocation api and encoding

--- weather_app/app.js
+++ weather_app/app.js
@@ -1,8 +1,22 @@
 const request = require('request');
+const yarg = require('yargs');
+const argv = yarg
+    .options({
+        a:{
+            demand : true,
+            alias : 'address',
+            description : "Address to fetch Weather for",
+            string : true
+        }
+}).argv;
+
+var addressURI = encodeURIComponent(argv.a);
 
 request({
-    url:'http://maps.googleapis.com/maps/api/geocode/json?address=%20abode%20valley%20chennai',
+    url:'http://maps.googleapis.com/maps/api/geocode/json?address='+addressURI,
     json: true
 },(error,response,body)=>{
-    console.log(body);
+    console.log(`address : ${body.results[0].formatted_address} `);
+    console.log(`latitude : ${body.results[0].geometry.location.lat}`);
+    console.log(`longitude : ${body.results[0].geometry.location.lng}`);    
 });
