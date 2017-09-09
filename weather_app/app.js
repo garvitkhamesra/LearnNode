b31ff3f6f285c30eb67ced1897865faebed1b7e9
Garvit Khamesra <garvitkhamesra@gmail.com>
2017-09-09 09:55:41 +0530

    weather app without promises

--- weather_app/app.js
+++ weather_app/app.js
@@ -2,35 +2,43 @@ const yarg = require('yargs');
 const geocode = require('./geocode/geocode.js');
 const weather = require('./weather/weather.js');
 
+const funcCall = (arg)=>{
+    geocode.geocodeAddress(arg,(errorMsg,result) => {
+        if(errorMsg){
+            console.log(errorMsg);
+        }
+        else if(result){
+            console.log("Address: ",result.address);
+            weather.getWeather(result.latitude,result.longitude,(errorMsg,result)=>{
+                if(errorMsg){
+                    console.log(errorMsg);        
+                }
+                else if(result){
+                    console.log(`Temperature : ${result.temperature}`);  
+                    console.log(`Apparent Temperature : ${result.apparentTemperature}`);                
+                }
+            });
+            
+        }
+    });
+};
+
 const argv = yarg
     .options({
         a:{
-            demand : true,
+            demand : false,
             alias : 'address',
             description : "Address to fetch Weather for",
             string : true
         }
 }).argv;
 
-geocode.geocodeAddress(argv.address,(errorMsg,result) => {
-    if(errorMsg){
-        console.log(errorMsg);
-    }
-    else if(result){
-        console.log("Address: ",result.address);
-
-        weather.getWeather(result.latitude,result.longitude,(errorMsg,result)=>{
-            if(errorMsg){
-                console.log(errorMsg);        
-            }
-            else if(result){
-                console.log(`Temperature : ${result.temperature}`);  
-                console.log(`Apparent Temperature : ${result.apparentTemperature}`);                
-            }
-        });
-        
-    }
-});
+if(argv.address){
+    funcCall(argv.address);
+}
+else{
+    funcCall("abode valley chennai");
+}
 
 
-//https://api.darksky.net/forecast/4961b0bf4df31a5db04c4ab4c79e339e/[latitude],[longitude]
+
