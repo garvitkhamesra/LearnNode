f575aaf6e97111cbbb5281760ed2e8f02d91b947
Garvit Khamesra <garvitkhamesra@gmail.com>
2017-09-14 00:53:30 +0530

    set up heroku

--- web_server/server.js
+++ web_server/server.js
@@ -1,11 +1,47 @@
 const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+hbs.registerPartials(__dirname + '/views/partials/');
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var log = `${new Date().toString()} : ${req.method} ${req.url}`
+    fs.appendFile('logs/server.log',log + '\n',(err)=>{
+        if(err){
+            console.log("unable to store");
+        }
+    });
+    next();
+});
+
+// app.use((req,res,next)=>{
+//     res.render('maintainence.hbs',{
+//        currentPage:"Maintainence Page" 
+//     });
+// });
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear(); 
+});
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
 
 app.get('/',(req,res)=>{
-    res.send("Hello");
+    res.render('home.hbs',{
+        currentPage : 'Home Page',
+        name : 'Garvit'
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        currentPage : 'About Page',
+    });
 });
 
 app.get('/bad',(req,res)=>{
@@ -15,6 +51,8 @@ app.get('/bad',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log("hi there server is on 3000");
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port,()=>{
+    console.log(`hi there server is on ${port}`);
 });
